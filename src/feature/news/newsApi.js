075354ba0b1d2dba8a297d560200cfcafc554fc5
mtagizadee <magatagi0@gmail.com>
2022-08-27 22:07:30 +0400

    News Page was initialized

--- src/feature/news/newsApi.js
+++ src/feature/news/newsApi.js
@@ -1,15 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const newsApi = createApi({
-        reducerPath: 'newsApi',
-        baseQuery: fetchBaseQuery({
-            baseUrl: 'http://localhost:5000/'
-        }),
-        endpoints: (builder) => ({
-            getAllNews: builder.query({
+    reducerPath: 'newsApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5000/'
+    }),
+    endpoints: (builder) => ({
+        getAllNews: builder.query({
             query: () => `news`,
         }),
+        getNewsById: builder.query({
+            query: (id) => `news/${id}`
+        })
     }),
 })
 
-export const { useGetAllNewsQuery } = newsApi
+export const { useGetAllNewsQuery, useGetNewsByIdQuery } = newsApi
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import LayOut from "../../components/layout";
+import {useNavigate, useParams} from "react-router";
+import {useGetNewsByIdQuery} from "../../feature/news/newsApi";
+import Loader from "../../components/Loader";
 
 const News = () => {
+    const {id} = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isNaN(Number(id))) {
+            navigate('/error');
+        }
+    },[]);
+
+    const {data, error, isLoading} = useGetNewsByIdQuery(id);
+
+    if (isLoading) return <Loader />
+    if (!data) navigate('/error');
+
     return (
         <LayOut>
           News
